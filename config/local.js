b776738c2f6b51792060e55d7443f131dc7fb953
Mohit Agarwal <mohit.agarwal019@gmail.com>
2014-06-08 13:22:41 +0000

    Mathml and Latex Support Added.

---
+++ config/local.js
@@ -0,0 +1,10 @@
+module.exports = {
+	    port: 8888,
+		environment: 'production',
+		          adapters: {
+				          mysql: {
+							             user: 'root',
+									               password: '12345'
+											               }
+						     }
+}
